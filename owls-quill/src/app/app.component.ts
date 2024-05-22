740fd29357ebb20bfec5231d0985e897c3980e33
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-05-22 12:47:16 -0500

    First draft of the frontend
    Primer borrador frontend

--- owls-quill/src/app/app.component.ts
+++ owls-quill/src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { NavbarComponent } from './components/navbar/navbar.component';
+import { HeroComponent } from './components/hero/hero.component';
+import { TestimonialsComponent } from './components/testimonials/testimonials.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NavbarComponent, TestimonialsComponent, HeroComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
