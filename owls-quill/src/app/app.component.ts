b9c2513966231ee48a3445ab163be76f1a48891b
Diego Carrion Portilla <dcportilla2003@gmail.com>
2024-05-25 19:58:30 -0500

    commit FrontEnd y Routes
    Se ha agregado la pagina del login asi como el landing page, se le ha dado funcionalidad al nav dandole rutas de inicio y de login. Tambien se ha modficiado la ruta para que cada vez que se habra la pagina web se rediriga automaticamente a /home

--- owls-quill/src/app/app.component.ts
+++ owls-quill/src/app/app.component.ts
@@ -4,11 +4,13 @@ import { NavbarComponent } from './components/navbar/navbar.component';
 import { HeroComponent } from './components/hero/hero.component';
 import { TestimonialsComponent } from './components/testimonials/testimonials.component';
 import { FooterComponent } from './components/footer/footer.component';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomeComponent } from './components/home/home.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, TestimonialsComponent, HeroComponent, FooterComponent],
+  imports: [RouterOutlet, NavbarComponent, TestimonialsComponent, HeroComponent, FooterComponent, LoginPageComponent, HomeComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- owls-quill/src/app/app.routes.ts
+++ owls-quill/src/app/app.routes.ts
@@ -1,3 +1,9 @@
 import { Routes } from '@angular/router';
+import { LoginPageComponent } from './components/login-page/login-page.component';
+import { HomeComponent } from './components/home/home.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', redirectTo: '/home', pathMatch: 'full' },
+    {path:"login", component: LoginPageComponent},
+    {path: "home", component: HomeComponent}
+];
